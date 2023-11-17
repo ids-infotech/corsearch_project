eebee5ac2613951baa1af2c93624aecc3d8d193a
GouravMi <80310399+GouravMi@users.noreply.github.com>
2023-11-17 08:15:45 +0530

    Update aruba_scanned_pdf_text_ext.py

--- aruba_scanned_pdf_text_ext.py
+++ aruba_scanned_pdf_text_ext.py
@@ -4,7 +4,6 @@ import pytesseract
 from PIL import Image
 from io import BytesIO
 import json
-import re
 
 def extract_text_from_pdf_with_images(pdf_file):
     text_per_page = []
@@ -21,20 +20,9 @@ def extract_text_from_pdf_with_images(pdf_file):
         # Save the Pixmap as a PNG image
         image = Image.frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples)
 
-        # Perform OCR on the image and extract text
-        ocr_text = pytesseract.image_to_string(image)
-        
-        # Extract lines starting with R-0 and use them as keys with their values
-        lines = ocr_text.split('\n')
-        extracted_data = {}
-        for line in lines:
-            match = re.match(r'^R-0\d+:(.*)$', line.strip())
-            if match:
-                key = match.group(0).split(':')[0].strip()
-                value = match.group(1).strip()
-                extracted_data[key] = value
-        
-        text_per_page.append({"page_number": page_num + 1, "extracted_data": extracted_data})
+        # Perform OCR on the image and extract text, splitting by newlines
+        ocr_text = pytesseract.image_to_string(image).split('\n')
+        text_per_page.append({"page_number": page_num + 1, "text": ocr_text})
 
     return text_per_page
 
@@ -47,11 +35,11 @@ def process_pdfs_in_folder(folder_path):
         for file in files:
             if file.lower().endswith(".pdf"):
                 pdf_file = os.path.join(root, file)
-                output_json_file = os.path.splitext(pdf_file)[0] + "_output_new.json"
+                output_json_file = os.path.splitext(pdf_file)[0] + "_output_1.json"
                 text_per_page = extract_text_from_pdf_with_images(pdf_file)
                 save_text_to_json(text_per_page, output_json_file)
                 print(f"Processed: {pdf_file}")
 
 if __name__ == '__main__':
-    folder_path = r'D:\python_projects\Corsearch\new files\Saint Vincent and the Grenadines VC\2023'  # Replace with the folder containing PDFs
+    folder_path = r'D:\python_projects\Corsearch\new files\Aruba AW'  # Replace with the folder containing PDFs
     process_pdfs_in_folder(folder_path)
