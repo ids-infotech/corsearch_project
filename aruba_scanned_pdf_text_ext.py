6d39e82c96248a0094f057f7acb5cdbfe4b60bf4
GouravMi <80310399+GouravMi@users.noreply.github.com>
2023-11-20 16:23:10 +0530

    Update aruba_scanned_pdf_text_ext.py

--- aruba_scanned_pdf_text_ext.py
+++ aruba_scanned_pdf_text_ext.py
@@ -1,17 +1,17 @@
-import os
 import fitz  # PyMuPDF
 import pytesseract
 from PIL import Image
-from io import BytesIO
 import json
+import os
 
 def extract_text_from_pdf_with_images(pdf_file):
-    text_per_page = []
+    text_per_page = {}
 
     # Open the PDF file using PyMuPDF
     pdf_document = fitz.open(pdf_file)
 
-    for page_num in range(pdf_document.page_count):
+    # Extract text starting from page 4
+    for page_num in range(3, pdf_document.page_count):
         page = pdf_document.load_page(page_num)
 
         # Convert the page to a Pixmap
@@ -20,9 +20,14 @@ def extract_text_from_pdf_with_images(pdf_file):
         # Save the Pixmap as a PNG image
         image = Image.frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples)
 
-        # Perform OCR on the image and extract text, splitting by newlines
-        ocr_text = pytesseract.image_to_string(image).split('\n')
-        text_per_page.append({"page_number": page_num + 1, "text": ocr_text})
+        # Perform OCR on the image and extract text
+        ocr_text = pytesseract.image_to_string(image)
+
+        # Remove empty lines and exclude headers and footers (customize as per your PDF layout)
+        lines = [line.strip() for line in ocr_text.split('\n') if line.strip()]
+        filtered_lines = [line for line in lines if not (line.startswith("Header") or line.endswith("Footer"))]
+
+        text_per_page[f"trademark{page_num + 1}"] = filtered_lines
 
     return text_per_page
 
@@ -35,7 +40,7 @@ def process_pdfs_in_folder(folder_path):
         for file in files:
             if file.lower().endswith(".pdf"):
                 pdf_file = os.path.join(root, file)
-                output_json_file = os.path.splitext(pdf_file)[0] + "_output_1.json"
+                output_json_file = os.path.splitext(pdf_file)[0] + "_output.json"
                 text_per_page = extract_text_from_pdf_with_images(pdf_file)
                 save_text_to_json(text_per_page, output_json_file)
                 print(f"Processed: {pdf_file}")
