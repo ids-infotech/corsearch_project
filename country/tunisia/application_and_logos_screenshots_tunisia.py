af35e5e0047dd9a7ca0d758aae5c02b671787985
abhaymunj <abhay@int-factor.tech>
2023-12-18 03:17:19 +0530

    Update
    Updated both scripts for tunisia with better function calling

--- country/tunisia/application_and_logos_screenshots_tunisia.py
+++ country/tunisia/application_and_logos_screenshots_tunisia.py
@@ -134,157 +134,159 @@ def process_lines(content_lines):
 
     return processed_lines
 
-# Define a regular expression pattern to match specific text sections
-pattern = re.compile(r"Numéro de dépôt[\s\S]*?(?=Numéro de dépôt|$)")
+def extract_and_segment_applications_tunisia(pdf_file_path):
+    # Define a regular expression pattern to match specific text sections
+    pattern = re.compile(r"Numéro de dépôt[\s\S]*?(?=Numéro de dépôt|$)")
 
-# # Path to the PDF file
-pdf_file_path = "TN20231005-455.pdf"
-pdf_document = fitz.open(pdf_file_path)
+    # # Path to the PDF file
+    # pdf_file_path = "TN20231005-440.pdf"
+    pdf_document = fitz.open(pdf_file_path)
 
-# Define the page range you want to process
-start_page = 10  # The first page in PyMuPDF is 0
-end_page = 30  # Enter page upto which we want to extract data
+    # Define the page range you want to process
+    start_page = 10  # The first page in PyMuPDF is 0
+    end_page = 30  # Enter page upto which we want to extract data
 
-# TO STORE IMAGE LOGOS
-output_dir_image_logo = f"{pdf_file_path}_image_logos"
+    # This dictionary will hold all extracted text with corresponding page numbers
+    text_pages = {}
+    # Loop over the specified range of pages
+    for page_num in range(start_page, end_page):
+        page = pdf_document[page_num]
+        text, pn = process_page_for_application_screenshots(page)
+        text_pages[pn] = text
 
-# This dictionary will hold all extracted text with corresponding page numbers
-text_pages = {}
-# Loop over the specified range of pages
-for page_num in range(start_page, end_page):
-    page = pdf_document[page_num]
-    text, pn = process_page_for_application_screenshots(page)
-    text_pages[pn] = text
+    # Concatenate the text from all pages
+    extracted_text = " ".join(text_pages.values())
 
-# Concatenate the text from all pages
-extracted_text = " ".join(text_pages.values())
+    # Find matches in the concatenated text
+    matches = pattern.findall(extracted_text)
 
-# Find matches in the concatenated text
-matches = pattern.findall(extracted_text)
+    # Create a dictionary to store section data and initialize section number
+    section_data = {}
+    section_num = 1
+    section_prefix = "tradeMark"
 
-# Create a dictionary to store section data and initialize section number
-section_data = {}
-section_num = 1
-section_prefix = "tradeMark"
+    # Initialize a list to store section content data
+    # section_content_data = []
 
-# Initialize a list to store section content data
-# section_content_data = []
+    # Iterate through the matches
+    for match in matches:
+        start_index = extracted_text.find(match)
 
-# Iterate through the matches
-for match in matches:
-    start_index = extracted_text.find(match)
-
-    # Determine the pages the match spans
-    # Create an empty list to store the page numbers where the match appears
-    page_nums = []
-    # Initialize a variable to keep track of the current position in the extracted text
-    current_pos = 0 
-    # Iterate through each page and its corresponding text
-    for pn, txt in text_pages.items():
-        # Update the current position by adding the length of the text on the current page
-        current_pos += len(txt)
-        if start_index < current_pos:
-            # If the starting index of the match is less than the current position, it means the match spans this page
-            # Add the page number to the list of page numbers where the match appears
-            # Increase the pn (page_numer) by 1 to match page number in PDF
-#             new_pn_matching_PDF = pn + 1
-            page_nums.append(pn)
-            # If the current position is greater than the end of the match, 
-            # it means the match doesn't span further pages
-            if current_pos > start_index + len(match):
-                break
+        # Determine the pages the match spans
+        # Create an empty list to store the page numbers where the match appears
+        page_nums = []
+        # Initialize a variable to keep track of the current position in the extracted text
+        current_pos = 0 
+        # Iterate through each page and its corresponding text
+        for pn, txt in text_pages.items():
+            # Update the current position by adding the length of the text on the current page
+            current_pos += len(txt)
+            if start_index < current_pos:
+                # If the starting index of the match is less than the current position, it means the match spans this page
+                # Add the page number to the list of page numbers where the match appears
+                # Increase the pn (page_numer) by 1 to match page number in PDF
+    #             new_pn_matching_PDF = pn + 1
+                page_nums.append(pn)
+                # If the current position is greater than the end of the match, 
+                # it means the match doesn't span further pages
+                if current_pos > start_index + len(match):
+                    break
 
 
-    #  NEW SECTION KEY
-    section_key = f"{section_prefix} {section_num}"
+        #  NEW SECTION KEY
+        section_key = f"{section_prefix} {section_num}"
 
-    if section_num not in section_data:
-        section_data[section_key] = {"page_number": [pn + 1 for pn in page_nums] # Adding 1 to adjust the page number
-                                     , "content": [],
-                                     "content_data": []
-                                    }
+        if section_num not in section_data:
+            section_data[section_key] = {"page_number": [pn + 1 for pn in page_nums] # Adding 1 to adjust the page number
+                                        , "content": [],
+                                        "content_data": []
+                                        }
 
-    # Split the matched text into lines and add them to the section data
-    content_lines = [line for line in match.split("\n") if line.strip()]
-    content_lines_cleaned =  process_lines(content_lines)
-    section_data[section_key]["content"].extend(content_lines_cleaned)
+        # Split the matched text into lines and add them to the section data
+        content_lines = [line for line in match.split("\n") if line.strip()]
+        content_lines_cleaned =  process_lines(content_lines)
+        section_data[section_key]["content"].extend(content_lines_cleaned)
 
-    # Initialize a pointer for the current start of the segment
-    segment_start = start_index
+        # Initialize a pointer for the current start of the segment
+        segment_start = start_index
 
-    # Temporary storage for the segmented content for this match
-    current_content_data = []
+        # Temporary storage for the segmented content for this match
+        current_content_data = []
 
-    # Iterate through the pages the match spans
-    for pn in page_nums:
-        # Determine where the segment for this page ends
-        segment_end = min(segment_start + len(text_pages[pn]), start_index + len(match))
+        # Iterate through the pages the match spans
+        for pn in page_nums:
+            # Determine where the segment for this page ends
+            segment_end = min(segment_start + len(text_pages[pn]), start_index + len(match))
 
-        # Check if segment_end is cutting a word. If yes, move it back to the last space.
-        while extracted_text[segment_end] not in [" ", "\n", "\t"] and segment_end > segment_start:
-            segment_end -= 1
+            # Check if segment_end is cutting a word. If yes, move it back to the last space.
+            while extracted_text[segment_end] not in [" ", "\n", "\t"] and segment_end > segment_start:
+                segment_end -= 1
 
-        # Extract the segment of the match that lies within this page
-        segment = extracted_text[segment_start:segment_end]
+            # Extract the segment of the match that lies within this page
+            segment = extracted_text[segment_start:segment_end]
 
-        # If this segment contains part of the match, add it to the current_content_data
-        if segment.strip():
+            # If this segment contains part of the match, add it to the current_content_data
+            if segment.strip():
+                
+                current_content_data.append({
+                    "text_on_page": pn + 1, # Adding 1 to adjust the page number
+                    "text": segment.strip()
+                })
+
+            # Update segment_start for the next page
+            segment_start = segment_end
+        
+        # Add the segmented content data to the section data
+        section_data[section_key]["content_data"].extend(current_content_data)
+        
+        # Call the correction function for the current section
+        #  Removing the HEADING from the extracted text
+        delimiter = " Muwassafat  N° 440 "
+        section_data[section_key]["content_data"] = correct_text_segmentation_for_application_screenshots(section_data[section_key]["content_data"], delimiter)
+        
+        # Call the merge_duplicate_pages_for_application_screenshots function to join text on the same page into one
+        section_data[section_key]["content_data"] = merge_duplicate_pages_for_application_screenshots(section_data[section_key]["content_data"])
+
+
+        # Split the text into lines (segments) for each entry in content_data
+        for entry in section_data[section_key]["content_data"]:
+            text = entry["text"]
+            lines = text.split('\n')
+
+            # Apply remove_leading_numbers and keep even the empty results
+            lines = [remove_leading_numbers(line) for line in lines]
             
-            current_content_data.append({
-                "text_on_page": pn + 1, # Adding 1 to adjust the page number
-                "text": segment.strip()
-            })
+            # Remove 'start' or 'stop' from the lines
+            lines = [line.replace('start', '').replace('stop', '') for line in lines]
+            # Filter out completely empty lines
+            lines = [line for line in lines if line.strip() != ""]
 
-        # Update segment_start for the next page
-        segment_start = segment_end
-    
-    # Add the segmented content data to the section data
-    section_data[section_key]["content_data"].extend(current_content_data)
-    
-    # Call the correction function for the current section
-    #  Removing the HEADING from the extracted text
-    delimiter = " Muwassafat  N° 455 "
-    section_data[section_key]["content_data"] = correct_text_segmentation_for_application_screenshots(section_data[section_key]["content_data"], delimiter)
-    
-    # Call the merge_duplicate_pages_for_application_screenshots function to join text on the same page into one
-    section_data[section_key]["content_data"] = merge_duplicate_pages_for_application_screenshots(section_data[section_key]["content_data"])
-
-
-    # Split the text into lines (segments) for each entry in content_data
-    for entry in section_data[section_key]["content_data"]:
-        text = entry["text"]
-        lines = text.split('\n')
-
-        # Apply remove_leading_numbers and keep even the empty results
-        lines = [remove_leading_numbers(line) for line in lines]
+            # Ensure there's at least one line to process
+            if not lines:
+                continue  # Skip this entry if there are no lines to process
+            # Call the function to remove 'start' or 'stop' from the ending index of 'text' in content_data
+            # remove_start_stop_from_text(section_data[section_key]["content_data"])
+            lines = remove_empty_strings(lines)
+            
+            # Update the "text" value for the current entry to contain the split lines
+            entry["text"] = lines
         
-        # Remove 'start' or 'stop' from the lines
-        lines = [line.replace('start', '').replace('stop', '') for line in lines]
-        # Filter out completely empty lines
-        lines = [line for line in lines if line.strip() != ""]
+        section_num += 1
 
-        # Ensure there's at least one line to process
-        if not lines:
-            continue  # Skip this entry if there are no lines to process
-        # Call the function to remove 'start' or 'stop' from the ending index of 'text' in content_data
-        # remove_start_stop_from_text(section_data[section_key]["content_data"])
-        lines = remove_empty_strings(lines)
-        
-        # Update the "text" value for the current entry to contain the split lines
-        entry["text"] = lines
-    
-    section_num += 1
+    # Serialize the section data to a JSON format
+    output_data = json.dumps(section_data, ensure_ascii=False, indent=4)
 
-# Serialize the section data to a JSON format
-output_data = json.dumps(section_data, ensure_ascii=False, indent=4)
+    output_file_path =  f'{pdf_file_path}_result_of_segmented_text.json'
 
-# Write the JSON data to a file
-with open(f"{pdf_file_path}_result_of_segmented_text.json", "w", encoding="utf-8") as json_file:
-    json_file.write(output_data)
+    # Write the JSON data to a file
+    with open(output_file_path, "w", encoding="utf-8") as json_file:
+        json_file.write(output_data)
+
+    return output_file_path
 
 
 '''TO GET COORDINATES FOR TUNISIA'''
-def define_coordinates_from_pdf(pdf_path, extracted_text_json_path, start_page=10, end_page= 30):   
+def define_coordinates_tunisia(pdf_path, extracted_text_json_path, start_page=10, end_page= 30):   
     
     # Load the JSON file containing the previously extracted text (if it exists)
     try:
@@ -411,13 +413,10 @@ def define_coordinates_from_pdf(pdf_path, extracted_text_json_path, start_page=1
         json.dump(existing_data, json_file, indent=4, ensure_ascii=False,  separators=(',', ': '))
 
 
-# Example usage:
-pdf_file_path = pdf_file_path  # Replace with the path to your PDF file, THIS VARIABLE IS IN THE STARTING OF THE SCRIPT
-extracted_text_json_path = f"{pdf_file_path}_result_of_segmented_text.json"  # This is the JSON input file and also the new output file
-define_coordinates_from_pdf(pdf_file_path, extracted_text_json_path)
+
 
 '''THIS PART TAKES IMAGES BASED'''
-def extract_and_process_images_tunisia(json_data, pdf_file, output_folder, resolution=200):
+def extract_and_process_images_tunisia(json_data, pdf_file, output_folder, output_parent_folder, resolution=200):
     # ADDING PADDING TO THE IMAGES TAKEN
     padding = 14.5
     # DICT TO STORE THE RECTS THAT WERE SKIPPED (MAINLY ONE LINERS)
@@ -425,6 +424,9 @@ def extract_and_process_images_tunisia(json_data, pdf_file, output_folder, resol
     last_processed_page = -1
 
     try:
+        # CREATE OUTPUT FOLDER PATH
+        output_folder = os.path.join(output_parent_folder, f"{pdf_path}_application_images")
+
         # CHECK IF AN OUTPUT FOLDER EXISTS, CREATE ONE IF IT DOES NOT EXIST
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
@@ -516,19 +518,10 @@ def extract_and_process_images_tunisia(json_data, pdf_file, output_folder, resol
         pdf_document.close()
 
 
-# Load the entire JSON file
-file_path = f'{pdf_file_path}_result_of_segmented_text.json'
-with open(file_path, 'r', encoding= 'utf-8') as file:
-    json_data = json.load(file)
-
-# Example usage
-pdf_file_path = pdf_file_path  # Replace with PDF file path
-output_folder_path = f'output_applications_image_{pdf_file_path}'  # Replace with output folder path
-extract_and_process_images_tunisia(json_data, pdf_file_path, output_folder_path)
 
 
 '''TO EXTRACT LOGOS IMAGES'''
-def extract_logos_bhutan(pdf_file_path, json_path, output_folder):
+def extract_logos_tunisia(pdf_file_path, json_path, output_folder, output_parent_folder):
     # Read JSON data
     with open(json_path, 'r', encoding='utf-8`') as file:
         data = json.load(file)
@@ -536,6 +529,8 @@ def extract_logos_bhutan(pdf_file_path, json_path, output_folder):
     # Open the PDF
     doc = fitz.open(pdf_file_path)
 
+    # CREATE OUTPUT FOLDER PATH
+    output_folder = os.path.join(output_parent_folder, f"{pdf_path}_logo_images")
     # Check if the output folder exists, if not, create it
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -594,14 +589,6 @@ def extract_logos_bhutan(pdf_file_path, json_path, output_folder):
     return data
 
 
-# Assuming the user will provide the path to the PDF file they want to process
-json_path = f'output_{pdf_file_path}.json'   # Replace this with the actual path to the JSON file
-# Set the path to the folder where images will be saved
-output_folder_logos = f"logo_images_{pdf_file_path}"  # Replace this with the actual path to the output folder
-
-# Extract images, save them in a folder, and update JSON
-updated_data_with_images_in_folder = extract_logos_bhutan(pdf_file_path, json_path, output_folder_logos)
-
 
 def update_trade_mark_keys_tunisia(input_file_path, output_file_path):
     # Read the JSON data from the file
@@ -621,11 +608,6 @@ def update_trade_mark_keys_tunisia(input_file_path, output_file_path):
     # Write the updated JSON data to a new file
     with open(output_file_path, 'w', encoding='utf-8') as file:
         json.dump(updated_json, file, indent=4, ensure_ascii=False)
-
-# Example usage
-input_file_path = json_path  # Replace with your input file path
-output_file_path = f"output_for_processing_{pdf_file_path}.json"  # Replace with your desired output file path
-update_trade_mark_keys_tunisia(input_file_path, output_file_path)
 
 
 def filter_json_data(input_file_path, output_file_path):
@@ -666,7 +648,46 @@ def filter_json_data(input_file_path, output_file_path):
     with open(output_file_path, 'w', encoding='utf-8') as file:
         json.dump(filtered_json, file, indent=4, ensure_ascii=False)
 
-# Example usage
-input_file_path = output_file_path  # Replace with your input file path
-output_file_path = input_file_path # Replace with your desired output file path
-filter_json_data(input_file_path, output_file_path)
+'''CALLING ALL THE OTHER FUNCTIONS'''
+def image_processing_tunisia_445(pdf_file_path):
+    # Step 1: Extract and segment applications
+    result_file_path = extract_and_segment_applications_tunisia(pdf_file_path)
+    print(f"Segmented data written to: {result_file_path}")
+
+    # Step 2: Define coordinates from PDF for application screenshots
+    extracted_text_json_path = f"{pdf_file_path}_result_of_segmented_text.json"
+    define_coordinates_tunisia(pdf_file_path, extracted_text_json_path)
+
+    # FOLDER WHERE THE IMAGES ARE SAVED
+    output_parent_folder = 'Tunisia_images'
+
+    # Step 3: Extract and process images
+    json_data = None
+    with open(extracted_text_json_path, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+
+    output_folder_path = f'output_applications_image_{pdf_file_path}'
+    extract_and_process_images_tunisia(json_data, pdf_file_path, output_folder_path, output_parent_folder)
+
+    # Step 4: Extract logos
+    json_path = f'output_{pdf_file_path}.json'
+    output_folder_logos = f"logo_images_{pdf_file_path}"
+    extract_logos_tunisia(pdf_file_path, json_path, output_folder_logos, output_parent_folder)
+
+    # Step 5: Update trade mark keys
+    input_file_path = json_path
+    output_file_path = f"output_for_processing_{pdf_file_path}.json"
+    update_trade_mark_keys_tunisia(input_file_path, output_file_path)
+
+    # Step 6: Filter JSON data
+    input_file_path = output_file_path
+    output_file_path = input_file_path
+    filter_json_data(input_file_path, output_file_path)
+
+
+if __name__ == "__main__":
+    # pdf_file_path = r'input_pdf\MN20230531-05.pdf'
+    # # Extracting just the filename
+    # pdf_file_name = os.path.basename(pdf_file_path)
+    pdf_path =  'TN20220722-440.pdf'
+    image_processing_tunisia_445(pdf_path)
